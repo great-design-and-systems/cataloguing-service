51e94bf9b4a6fd839357e1ebd0a59bb4e9d31966
rickzx98 <rickzx98@gmail.com>
2017-02-26 13:21:28 +0800

    update

--- src/control/marc/parse-field-value.js
+++ src/control/marc/parse-field-value.js
@@ -1,4 +1,5 @@
 import lodash from 'lodash';
+import GetFormattedValue from './get-formatted-value'
 export default class ParseFieldValue {
     constructor(keys, context, marcFormat, callback) {
         this.parseKeys(keys, context, {}, marcFormat, (err, result)=> {
@@ -48,72 +49,4 @@ export default class ParseFieldValue {
             value: getFormattedValue.getValue()
         };
     }
-}
-class GetFormattedValue {
-    constructor(value, valueFormat, key) {
-        let keyFormat = lodash.find(valueFormat, (valF, fieldV)=> {
-            this.field = fieldV;
-            return valF === key
-        });
-        if (keyFormat) {
-            this.formattedValue = value;
-        } else {
-            this.field = key;
-            let valueArray = value.split('');
-            if (valueArray && valueArray.length) {
-                this.formattedValue = {};
-                lodash.forEach(valueFormat, (formatV, formatF)=> {
-                    this.formattedValue[formatF] = {};
-                    if (formatV instanceof Array) {
-                        this.formattedValue[formatF] = this.parseArrayFieldObject(formatV, value);
-                    } else {
-                        this.formattedValue[formatF] = value.charAt(formatV);
-                    }
-                });
-            }
-        }
-    }
-
-    parseArrayFieldObject(fieldObject, value) {
-        let result = {};
-        const start = fieldObject[0];
-        let end;
-        let format;
-        if (fieldObject[1] instanceof Object) {
-            format = fieldObject[1];
-        } else {
-            end = fieldObject[1];
-        }
-        if (fieldObject.length > 2) {
-            format = fieldObject[2];
-        }
-        value = end ? value.substr(start, end) : value.charAt(start);
-        if (format) {
-            if (format instanceof Array) {
-                format.forEach((label, index)=> {
-                    result[label] = value.charAt(index);
-                });
-            } else {
-                lodash.find(format, (fmtValue, field)=> {
-                    let matched = fmtValue === value;
-                    if (matched) {
-                        result = field;
-                    }
-                    return matched;
-                });
-            }
-        } else {
-            result = value;
-        }
-        return lodash.isEmpty(result) ? undefined : result;
-    }
-
-    getValue() {
-        return this.formattedValue;
-    }
-
-    getField() {
-        return this.field;
-    }
-
 }
--- src/control/marc/parse-marc-value.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by rickzx98 on 24/02/2017.
- */
